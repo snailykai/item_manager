88db3d46a26f680b46207d48bc8ae4f418d0e7f7
kai <snailsmilesnaily@gmail.com>
2023-05-19 16:37:50 +0900

    findAll method

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,40 +11,64 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.entity.Item;
 import com.example.form.ItemForm;
+import com.example.service.ItemService;
 
 @Controller
 @RequestMapping("/item")
 public class ItemController {
+    private final ItemService itemService;
 
-	@GetMapping("toroku")
-	public String index(Model model) {
-		return "item/index";
-	}
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
 
-	@GetMapping("toroku")
-	public String torokuPage(@ModelAttribute("itemForm") ItemForm itemForm) {
-		return "item/torokuPage";
-	}
+    // 商品一覧の表示
+    @GetMapping
+    public String index(Model model) {
+    	List<Item> items = this.itemService.findAll();
+    	System.out.println(items.toString());
+    	model.addAttribute("items", items);
+        return "item/index";
+    }
 
-	@PostMapping("toroku")
-	public String toroku(ItemForm itemForm) {
-		return "redirect:/item";
-	}
+    // 商品登録ページ表示用
+    @GetMapping("toroku")
+    public String torokuPage(@ModelAttribute("itemForm") ItemForm itemForm) {
+        // 処理を追加
+        return "item/torokuPage";
+    }
 
-	@GetMapping("henshu/{id}")
-	public String henshuPage(@PathVariable("id") Integer id, Model model, @ModelAttribute("itemForm") ItemForm itemForm) {
-		return "item/henshuPage";
-	}
+    // 商品登録の実行
+    @PostMapping("toroku")
+    public String toroku(ItemForm itemForm) {
+        // 処理を追加
+        return "redirect:/item";
+    }
 
-	@PostMapping("henshu/{id}")
-	public String henshu(@PathVariable("id") Integer id, @ModelAttribute("itemForm") ItemForm itemForm) {
-		return "redirect:/item";
-	}
+    // 商品編集ページ
+    @GetMapping("henshu/{id}")
+    public String henshuPage(@PathVariable("id") Integer id, Model model
+                             , @ModelAttribute("itemForm") ItemForm itemForm) {
+        // 処理を追加
+        return "item/henshuPage";
+    }
 
-	@PostMapping("sakujo/{id}")
-	public String sakujo(@PathVariable("id") Integer id) {
-		return "redirect:/item";
-	}
+    // 商品編集の実行
+    @PostMapping("henshu/{id}")
+    public String henshu(@PathVariable("id") Integer id, @ModelAttribute("itemForm") ItemForm itemForm) {
+        // 処理を追加
+
+        return "redirect:/item";
+    }
+
+    // 商品削除の実行
+    @PostMapping("sakujo/{id}")
+    public String sakujo(@PathVariable("id") Integer id) {
+        // 処理を追加
+        return "redirect:/item";
+    }
 
 }
